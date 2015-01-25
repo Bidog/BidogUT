01cad5c98218ce36829cd9358910346817784c18
wuyue <mason.liu@sensoriainc.com>
2015-01-25 12:49:52 -0500

    small large screen

---
+++ WuyueP2/app/src/main/java/uoft/wuyuep2/LaunchPage.java
@@ -0,0 +1,133 @@
+package uoft.wuyuep2;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link LaunchPage.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link LaunchPage#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LaunchPage extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment LaunchPage.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static LaunchPage newInstance(String param1, String param2) {
+        LaunchPage fragment = new LaunchPage();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public LaunchPage() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_launch_page, container, false);
+        Button mEnterNameButton = (Button) view.findViewById(R.id.EnterName_button);
+        mEnterNameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // here you set what you want to do when user clicks your button,
+                Log.d("button", "I just click the button");
+                EnterName_Fragment fragment2 = new EnterName_Fragment();
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                if(isTablet(getActivity().getBaseContext())){
+                    fragmentTransaction.replace(R.id.right_fragment_container, fragment2);
+                }else {
+                    fragmentTransaction.replace(R.id.left_fragment_container, fragment2);
+                }
+                fragmentTransaction.commit();
+            }
+        });
+
+
+
+
+        return view;
+    }
+
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(Uri uri);
+    }
+    public static boolean isTablet(Context context) {
+        return (context.getResources().getConfiguration().screenLayout
+                & Configuration.SCREENLAYOUT_SIZE_MASK)
+                >= Configuration.SCREENLAYOUT_SIZE_LARGE;
+    }
+
+}
---
+++ WuyueP2/app/src/main/java/uoft/wuyuep2/MainActivity.java
@@ -0,0 +1,147 @@
+package uoft.wuyuep2;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+
+
+public class MainActivity extends ActionBarActivity {
+
+
+    private final QuotesFragment mQuoteFragment = new QuotesFragment();
+    public static String[] mTitleArray;
+    public static String[] mQuoteArray;
+
+
+  //  private final QuotesFragment mQuoteFragment = new QuotesFragment();
+    private FragmentManager mFragmentManager;
+    private FrameLayout mLeftFrameLayout, mRightFrameLayout;
+
+    private static final int MATCH_PARENT = LinearLayout.LayoutParams.MATCH_PARENT;
+    private static final String TAG = "QuoteViewerActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        Log.i(TAG, getClass().getSimpleName() + ":entered onCreate()");
+
+        super.onCreate(savedInstanceState);
+
+        // Get the string arrays with the titles and qutoes
+
+
+        setContentView(R.layout.main);
+
+        // Get references to the TitleFragment and to the QuotesFragment
+        mLeftFrameLayout = (FrameLayout) findViewById(R.id.left_fragment_container);
+        mRightFrameLayout = (FrameLayout) findViewById(R.id.right_fragment_container);
+
+
+        // Get a reference to the FragmentManager
+        mFragmentManager = getFragmentManager();
+
+        // Start a new FragmentTransaction
+        FragmentTransaction fragmentTransaction = mFragmentManager
+                .beginTransaction();
+
+        // Add the TitleFragment to the layout
+        fragmentTransaction.add(R.id.left_fragment_container,
+                new LaunchPage());
+
+        // Commit the FragmentTransaction
+        fragmentTransaction.commit();
+
+        // Add a OnBackStackChangedListener to reset the layout when the back stack changes
+        mFragmentManager
+                .addOnBackStackChangedListener(new FragmentManager.OnBackStackChangedListener() {
+                    public void onBackStackChanged() {
+                        setLayout();
+              }
+        });
+        if (isTablet(getBaseContext())){
+            Log.d("here","this is a large screen");
+            // Start a new FragmentTransaction
+            fragmentTransaction = mFragmentManager
+                    .beginTransaction();
+            // Add the QuoteFragment to the layout
+            fragmentTransaction.add(R.id.right_fragment_container,
+                    mQuoteFragment);
+            // Add this FragmentTransaction to the backstack
+            fragmentTransaction.addToBackStack(null);
+            // Commit the FragmentTransaction
+            fragmentTransaction.commit();
+            // Force Android to execute the committed FragmentTransaction
+            mFragmentManager.executePendingTransactions();
+        }
+    }
+
+
+    private void setLayout() {
+
+        // Determine whether the QuoteFragment has been added
+        if (!isTablet(getBaseContext())) {
+
+            // Make the TitleFragment occupy the entire layout
+            mLeftFrameLayout.setLayoutParams(new LinearLayout.LayoutParams(
+                    MATCH_PARENT, MATCH_PARENT));
+            mRightFrameLayout.setLayoutParams(new LinearLayout.LayoutParams(0,
+                    MATCH_PARENT));
+        } else {
+            // Make the TitleLayout take 1/3 of the layout's width
+            mLeftFrameLayout.setLayoutParams(new LinearLayout.LayoutParams(0,
+                    MATCH_PARENT, 1f));
+
+            // Make the QuoteLayout take 2/3's of the layout's width
+            mRightFrameLayout.setLayoutParams(new LinearLayout.LayoutParams(0,
+                    MATCH_PARENT, 2f));
+        }
+    }
+    public boolean isTablet(Context context) {
+        boolean xlarge = ((context.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == 4);
+        boolean large = ((context.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_LARGE);
+        return (xlarge || large);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        Log.i(TAG, getClass().getSimpleName() + ":entered onDestroy()");
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.i(TAG, getClass().getSimpleName() + ":entered onPause()");
+        super.onPause();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.i(TAG, getClass().getSimpleName() + ":entered onRestart()");
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.i(TAG, getClass().getSimpleName() + ":entered onResume()");
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        Log.i(TAG, getClass().getSimpleName() + ":entered onStart()");
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.i(TAG, getClass().getSimpleName() + ":entered onStop()");
+        super.onStop();
+    }
+}
